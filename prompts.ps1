2c40b92c5c59af47c78fd763dedc0b0c16f6955e
Liam E <liamescusa@gmail.com>
2025-06-02 12:23:09 -0700

    Update prompts.ps1

--- prompts.ps1
+++ prompts.ps1
@@ -1,28 +1,105 @@
-#Liam Esucsa     #Student ID 011950351
+<#Liam Esucsa     Student ID 011950351#>
 
-#Begin User Prompt Menu
-do {
-    Write-Host "Menu"
-    Write-Host "1) List .log within Requirements1 folder"
-    Write-host "2) List Requirements 1 folder in alphabetical order"
-    Write-host "3) List cpu and memory usage"
-    Write-host "4) List different running processes by greatest to least virtual size"
-    Write-Host "5) Exit script execution"
+#Region Functions
+function Show-Menu {
+   
+    param (
+        [string]$Title = "Main Menu",
+        [string[]]$Options
+   ) 
 
-#get user input
-switch ($choice) {
-    1 { #B1. List files within Requirements1 folder and redirect results to DailyLog.txt
-        $logfiles = Get-ChildItem -Filter "*.log" | Select-Object -ExpandProperty Name
-        $logfilesdate = "$(Get-Date) - $(logfiles -join ', ')"
-        #append to dailylog.txt without overwriting
-        Add-Content -Path ".\DailyLog.txt" -Value $logfilesdate
-        Write-Host "Log files listed and append to DailyLog.txt."
+    Write-Verbose "Running Show-Menu Function"
+
+    Write-Host $Title
+    Write-host "==========="
+    # Create a loop for 5 options
+    for($i - 0; $i -lt $Options.Length; $i++)
+    {
+            #Iterate over the array and write the selection number
+            Write-Host "$($i+1). $($Options[$i])"
     }
 
-    2{
+    # Prompt User For Input
+    $selection = Read-Host "Please select an option"
 
+    # Validate the input with RegEx
+    if ($selection -match '^\d+$' -and $selection -le $Options.Length)
+    {
+            #Return the selection
+            return $selection
     }
-    condition {  }
-    Default {}
+    else {
+        # if the input is invalid, inform the user and re-prompt
+        Write-Host -ForegroundColor DarkYellow "'n*Invalid selection. Please try again*'n"
+        Show-Menu -Title $Title -Options $Options
+    }
+
 }
-69CE586EB0A3AF3B1A056F768822A1DA02E5646E589132EC251F1E235C9C74AB
+   
+#EndRegion
+
+#Main Script Logic
+$menuOptions = @("List files in Requirements1 (.log), List files in Requirements1 (alphabetical), List CPU and Memory Usage, List Running Processes, Exit")
+
+
+$UserInput = 1
+
+
+    try {
+        while($UserInput -ne 5)
+        {
+            $UserInput = Show-Menu -Title "Please Select a Number" -Options $menuOptions
+
+            switch ($UserInput) 
+            {
+                1
+                {
+                    #List files within Requirements1 folder and redirect results to DailyLog.txt
+                    $logfiles = Get-ChildItem -Filter "*.log" | Select-Object -ExpandProperty Name
+                    $logfilesdate = "$(Get-Date) - $(logfiles -join ', ')"
+                    #append to dailylog.txt without overwriting
+                    Add-Content -Path ".\DailyLog.txt" -Value $logfilesdate
+                    Write-Host "Log files listed and append to DailyLog.txt."
+                }
+                
+                2{ 
+                    #List files in Requirements1 folder (alphabetical)
+                }
+
+                3{
+                    #List CPU and Memory Usage
+                }
+
+                4{
+                    #List running process by virtual size
+                }
+
+                5{
+                    #Exit Program
+                    Write-Verbose "Option 5 Selected"
+                    Write-Host -ForegroundColor Cyan "Thank you. Have a great day!"
+                }
+            }
+
+        }
+    }
+    catch [System.OutOfMemoryException]
+    {
+        Write-Host -ForegroundColor Red "ERROR"
+        $_
+    }
+    catch
+    {
+        Write-Host -ForegroundColor Red "ERROR"
+        $_
+    }
+    try {
+        
+    }
+    finally 
+    {
+        # Close any open resource
+    }
+
+
+D1CF39306E9816558892EE6B2BC7ED6358E0539D2E3BAC94C6F518F97517EBFC
